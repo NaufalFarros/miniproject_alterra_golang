ce41f021ed01bb44a1fa1993da07b565c9c93524
Naufal Farros <71809599+NaufalFarros@users.noreply.github.com>
2023-04-28 21:57:24 +0700

    update

---
+++ routes/admin.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+
+func routes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+}
